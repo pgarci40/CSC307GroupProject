71098c4cdb993cf8e384318852d5fbc96ac2ab0c
Alyssa Gerardo <alyssagerardo2005@gmail.com>
2025-10-28 17:14:40 -0700

    minor changes to database connection to backend

--- packages/express-backend/inventory-services.js
+++ packages/express-backend/inventory-services.js
@@ -8,6 +8,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
+  .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log(error));
 
 function getStores(name, location){
